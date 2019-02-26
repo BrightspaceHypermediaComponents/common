b15f90c370ac19465870ea6a8b87840023e6029b
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2019-02-26 18:04:22 -0500

    Add HM skeleton stuff

--- components/d2l-hm-filter/d2l-hm-filter.js
+++ components/d2l-hm-filter/d2l-hm-filter.js
@@ -1,4 +1,6 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import 'd2l-polymer-siren-behaviors/store/entity-behavior.js';
+import {mixinBehaviors} from '@polymer/polymer/lib/legacy/class.js';
 
 /**
  * `d2l-hm-filter`
@@ -9,7 +11,7 @@ import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
  * @demo demo/d2l-hm-filter/d2l-hm-filter.html
  */
 
-class D2LHypermediaFilter extends PolymerElement {
+class D2LHypermediaFilter extends mixinBehaviors([D2L.PolymerBehaviors.Siren.EntityBehavior], PolymerElement) {
 	static get template() {
 		return html`
 			<style>
